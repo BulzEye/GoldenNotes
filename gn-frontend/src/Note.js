b4ee5f3c73513971561fd488986561ad63724ef6
BulzEye <shirgaonkarom@yahoo.in>
2021-10-29 20:41:57 +0530

    Grid layout + click on note instead of text

--- gn-frontend/src/Note.js
+++ gn-frontend/src/Note.js
@@ -1,13 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 const Note = (props) => {
+    const history = useHistory();
+    const loadNote = () => {
+        console.log("Note loaded");
+        history.push(`/editnote/${props.content._id}`);
+        // history.push("/");
+    }
+
     return ( 
         
-        <div className="note">
-            <Link to={`/editnote/${props.content._id}`}>
-                <h3> {/* Note heading */} {props.content.title} </h3>
-                <p> { /* Note body */ } {props.content.body} </p>
-            </Link>
+        <div className="note" onClick={loadNote}>
+            {/* <Link to={`/editnote/${props.content._id}`}> */}
+            <h3> {/* Note heading */} {props.content.title} </h3>
+            <p> { /* Note body */ } {props.content.body} </p>
+            {/* </Link> */}
         </div>
      );
 }
