36d2ca3c3b0445c8a05ab3b7efc00cf76bf02f98
BulzEye <shirgaonkarom@yahoo.in>
2022-06-05 20:03:22 +0530

    fix syntax error in local devserver command

--- devServer.js
+++ devServer.js
@@ -1,3 +1,3 @@
-const args = [ 'devstart' ];
+const args = [ 'run devstart' ];
 const opts = { stdio: 'inherit', cwd: 'gn-backend', shell: true };
 require('child_process').spawn('npm', args, opts);
