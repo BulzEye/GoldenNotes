5f87b56d096bc9d942090918e71bfaf44dbf8196
BulzEye <shirgaonkarom@yahoo.in>
2022-06-05 21:57:20 +0530

    added cors package to backend

--- gn-backend/server.js
+++ gn-backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const apiRoutes = require("./routes/apiRoutes");
 
@@ -8,6 +9,13 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
+
+var corsOptions = {
+    origin: 'https://goldennotes.netlify.com',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+
+app.use(cors(corsOptions));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
