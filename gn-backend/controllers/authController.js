2db75ff07e531560392954e1897c976b14c9f745
BulzEye <shirgaonkarom@yahoo.in>
2022-07-26 18:42:47 +0530

    Added auth code for login and signup in backend

--- gn-backend/controllers/authController.js
+++ gn-backend/controllers/authController.js
@@ -1,12 +1,68 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const errorHandler = (err) => {
+    console.log(err);
+    let errors = {email: "", password: ""};
+    if(err.code === 11000) {
+        errors.email = "This email is already registered. Log in instead.";
+        return errors
+    }
+    if(err.message.includes("User validation failed")) {
+        console.log("Received error");
+        Object.values(err.errors).forEach(({properties})=> {
+            errors[properties.path] = properties.message;
+        });
+    }
+    if(err.message === "User does not exist") {
+        errors.email = err.message;
+    }
+    if(err.message === "Wrong password entered") {
+        errors.password = err.message;
+    }
+    return errors;
+}
+
+const maxAge = 30*24*60*60;
+const createToken = (id) => {
+    return jwt.sign({id}, "BulzEye secret", {
+        expiresIn: maxAge
+    });
+}
 
 module.exports.login_post = (req, res) => {
     console.log("Received POST request for logging in");
     console.log(req.body);
-    res.send("Received request");
+
+    User.login(req.body.email, req.body.password)
+    .then((user) => {
+        const token = createToken(user._id);
+        res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge*1000 });
+        res.status(201).send({ user: user._id });
+    })
+    .catch((err) => {
+        // console.log(err);
+        const errors = errorHandler(err);
+        res.status(400).send({errors});
+    })
+    // res.send("Received request");
 };
 
-module.exports.signup_post = (req, res) => {
+module.exports.signup_post = async (req, res) => {
     console.log("Received POST request for signing up");
-    res.send("Received request");
+
+    const { email, password } = req.body;
+    User.create({email, username: email.substring(0, email.indexOf("@")) , password})
+    .then((user) => {
+        // res.status(201).send(user);
+        const token = createToken(user._id);
+        res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge*1000 });
+        res.status(201).send({ user: user._id });
+    })
+    .catch((err) => {
+        const errors = errorHandler(err);
+        res.status(400).send({errors});
+    });
+
+    // res.send("Received request");
 };
