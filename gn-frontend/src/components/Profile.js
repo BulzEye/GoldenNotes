3eff314a362ade8741f6eecbabd49d324794b89d
BulzEye <shirgaonkarom@yahoo.in>
2022-10-03 00:56:39 +0530

    replaced placeholder data with user details

--- gn-frontend/src/components/Profile.js
+++ gn-frontend/src/components/Profile.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useUserContext } from "../hooks/useUserContext";
 import "./Profile.css";
 
 const Profile = (props) => {
+    const { user } = useUserContext();
     const { logout } = useLogout();
 
     return ( 
@@ -12,13 +14,13 @@ const Profile = (props) => {
                 <div className="profileEntry">
                     <span className="profileLabel">Username</span>
                     <div className="profileContent">
-                        bulzEye
+                        {user.username}
                     </div>
                 </div>
                 <div className="profileEntry">
                     <span className="profileLabel">Email</span>
                     <div className="profileContent">
-                        shirgaonkarom@yahoo.in
+                        {user.email}
                     </div>
                 </div>
                 <Link to={"/"} onClick={logout}>
